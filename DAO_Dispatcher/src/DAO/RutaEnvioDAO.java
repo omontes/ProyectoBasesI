7cac9dc647e17a13bfe6330f128699a1bfa5e807
omontes <pulgamontes@gmail.com>
2014-08-13 18:23:22 -0600

    Creando el DAO Dispatcher

---
+++ DAO_Dispatcher/src/DAO/RutaEnvioDAO.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+import DTO.RutaEnvioDTO;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Oscar Montes
+ */
+public class RutaEnvioDAO {
+    private Connection conexion;
+    private Statement statement;
+    private static RutaEnvioDAO AdminBD;
+    public RutaEnvioDAO(Connection conection, Statement statement) {
+        this.conexion = conection;
+        this.statement = statement;
+
+    }
+
+    public static RutaEnvioDAO getInstance() throws SQLException, NamingException {
+        
+        InitialContext ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup("jdbc/cargodispatcher");
+        Connection con = ds.getConnection();
+        AdminBD = new RutaEnvioDAO(con, con.createStatement());
+
+        return AdminBD;
+    }
+    
+    /******* OPERACIONES BASICAS QUE SE REALIZAN A LA BASE DE DATOS *******/
+    
+    /**
+     * Este metodo realiza una consulta a la tabla de Departamentos y obtiene
+     * la informacion Dname, Dno y Total_sal
+     */ 
+    public ArrayList<RutaEnvioDTO> consultarRutas() throws Exception {
+        ArrayList<RutaEnvioDTO> lsitaRutas = new ArrayList<RutaEnvioDTO>();
+        try {
+            String consultarRutas= this.leerSQL("/ArchivosSQL/ConsultaRutas.sql");
+            ResultSet rs = statement.executeQuery(consultarRutas);
+            while (rs.next()) {
+                RutaEnvioDTO rutaEnvio = new RutaEnvioDTO();
+                rutaEnvio.setNombre(rs.getString("nombre"));
+                rutaEnvio.setIdRutaEnvio(rs.getInt("idRutaEnvio"));
+                lsitaRutas.add(rutaEnvio);
+            }  
+            statement.close();
+            return lsitaRutas;
+            
+
+
+        } catch (Exception e) {
+            System.out.println("Error al realizar la consulta de contenedores");
+            throw(e);
+
+        }
+
+    }
+    
+      /**
+     * Realiza la lectura del archivo sql
+     * @param direccion
+     * @return
+     * @throws IOException 
+     */
+    private String leerSQL(String direccion) throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputfile = classLoader.getResourceAsStream(direccion);
+        StringBuilder sb;
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(inputfile))) {
+            String str;
+            sb = new StringBuilder();
+            while ((str = input.readLine()) != null) {
+                sb.append(str).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+}
