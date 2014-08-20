85df5a07d01ad665d9bf2654d47ee2b8aa6b62e3
omontes <pulgamontes@gmail.com>
2014-08-19 22:41:12 -0600

    Se arreglaron unos errores de las consultas!

--- CargoDispatcher/src/java/ArchivosSQL/ConsultaRutas.sql
+++ CargoDispatcher/src/java/ArchivosSQL/ConsultaRutas.sql
@@ -1,2 +1,3 @@
 SELECT R.`idRutaEnvio`,R.nombre,D.pto_llegada,D.pto_salida,R.tiempo_dias AS tiempo,R.costo,R.maximocontenedor
-FROM rutaenvio AS R, direccionruta AS D;
+FROM rutaenvio AS R, direccionruta AS D
+WHERE R.direccion_ruta=D.`idDireccionRuta`;
