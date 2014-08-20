7ade2b37bedfbcc6257acef57895396c968770a4
omontes <pulgamontes@gmail.com>
2014-08-19 23:21:26 -0600

    Se arreglo un bug de la consulta clientes, le falto el idCliente

--- CargoDispatcher/src/java/ArchivosSQL/ConsultaClientes.sql
+++ CargoDispatcher/src/java/ArchivosSQL/ConsultaClientes.sql
@@ -1,3 +1,3 @@
-SELECT C.nombre,C.telefono,C.`idRutaEnvio`,C.puntos,T.`Descripcion` AS tipo, C.numero_cuenta
+SELECT C.`idCliente`,C.nombre,C.telefono,C.`idRutaEnvio`,C.puntos,T.`Descripcion` AS tipo, C.numero_cuenta
 FROM cliente AS C, tipocliente AS T
 WHERE C.tipo_cliente=T.`idTipoCliente`;
