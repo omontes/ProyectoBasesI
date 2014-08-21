13ca2c2e259e9d856c19906f30d6e2157e013ef2
M. Taylor <M. Taylor@192.168.2.8>
2014-08-21 02:56:39 -0600

    github es una basura y me obliga a hacer comit de cosas que yo no toque

---
+++ cliente/web/jQueryClient.js
@@ -0,0 +1,42 @@
+ // The root URL for the RESTful services
+var rootURL = "http://192.168.1.108:8080/CargoDispatcher/webresources";
+
+// Retrieve Employees list when application starts 
+getClientes();
+
+
+
+function findAll() {
+	console.log('findAll');
+	$.ajax({
+		type: 'GET',
+		url: rootURL + "/cliente/getClientes",
+		dataType: "json", // data type of response
+		success: function(data){
+                        $('#h1').append("Consulta Realizada");
+			renderList(data);
+		}
+	});
+}
+function getClientes() {
+    //console.log('getClientes');
+    $.ajax({
+        
+        type: 'GET',
+        url: rootURL + "/cliente/getClientes",
+        dataType: "json",
+        success: function(data) {
+            $('#h1').append("Consulta Realizada");
+            //renderClientes(data);
+        }
+    });
+}
+function renderList(data) {
+        $('#h1').append(data[0].descripcion);
+        //var list = JSON.parse(data);
+	//$('#employeeList li').remove();
+        //var i;
+        //for(i = 0; i < arr.length; i++) {
+        //    $('#employeeList').append('<li><a href="#" data-identity="' + list[0].Ssn + '">'+ list[1].Name+'</a></li>');
+        //}
+    }
