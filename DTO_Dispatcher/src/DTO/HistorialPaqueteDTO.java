6bd4d0c78f32bf3941bbba16ee93d6f2b76c8b29
omontes <pulgamontes@gmail.com>
2014-08-22 23:19:36 -0600

    tracking #11 #6 Listo

---
+++ DTO_Dispatcher/src/DTO/HistorialPaqueteDTO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DTO;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Oscar Montes
+ */
+public class HistorialPaqueteDTO {
+     private String idPaquete;
+     private Date fecha;
+     private String estado;
+
+    /**
+     * @return the idPaquete
+     */
+    public String getIdPaquete() {
+        return idPaquete;
+    }
+
+    /**
+     * @param idPaquete the idPaquete to set
+     */
+    public void setIdPaquete(String idPaquete) {
+        this.idPaquete = idPaquete;
+    }
+
+   
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+     
+}
