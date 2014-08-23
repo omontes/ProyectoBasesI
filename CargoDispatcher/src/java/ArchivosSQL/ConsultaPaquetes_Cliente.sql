0a1900f9191eb628bfc5b8a0e92b03bf74a7d7d7
omontes <pulgamontes@gmail.com>
2014-08-23 14:11:58 -0600

    Se arreglo lo de los parametros esta funcionando perfectamente!!

--- CargoDispatcher/src/java/ArchivosSQL/ConsultaPaquetes_Cliente.sql
+++ CargoDispatcher/src/java/ArchivosSQL/ConsultaPaquetes_Cliente.sql
@@ -1,4 +1,4 @@
-SELECT P.`idPaquete`, P.descripcion, E.`Descripcion` AS EstadoActual
+SELECT P.`idPaquete`, P.descripcion, E.`Descripcion` AS EstadoActual,P.peso AS peso
 FROM cliente AS C, paquete AS P, historialpaquete AS H, estadospaquete AS E
 WHERE C.`idCliente`=P.`idCliente` AND H.`idPaquete`=P.`idPaquete` AND H.`idEstado`=E.`idEstadosPaquete` AND C.`idCliente`=?
 AND H.`Fecha` = (SELECT  MAX(historialpaquete.`Fecha`) FROM historialpaquete WHERE historialpaquete.`idPaquete`=H.`idPaquete`);
